5008a47f2ca548abcdb62b32d6cdd67cd0dec9b0
HiteshiAtkare <atkareh29@gmail.com>
2023-07-14 13:36:56 +0530

    add

--- interface.py
+++ interface.py
@@ -52,6 +52,6 @@ def Admit_chance():
     return jsonify({'Neither':'POST & GET'})
     
 if __name__=="__main__":              
-    app.run(host="0.0.0.0",port=5001,debug=True)
+    app.run(host="0.0.0.0",port=8080)
 
     
